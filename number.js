e69fd4ba95a43e6933c624c16e8cc27cbf2dd2d9
shahriaralam465 <shahriaralam465@gamil.com>
2024-01-28 13:35:54 +0600

    added changes

--- number.js
+++ number.js
@@ -1,8 +1,16 @@
 var orangePrice = 20;
 var chocolate = 0.5 ;
-var applePrice =parseInt('20') ;
+// var applePrice =parseInt('20') ;
+var applePrice =parseFloat('22.5') ;
 
-console.log(typeof orangePrice)
-console.log(typeof applePrice)
-console.log(orangePrice + applePrice);
+// console.log(typeof orangePrice)
+// console.log(typeof applePrice)
+// console.log(orangePrice + applePrice);
 
+var first = 0.1;
+var second = 0.2;
+var total =first + second;
+// console.log(total.toFixed(3))
+
+const num = 50/4 ;
+console.log(num);
