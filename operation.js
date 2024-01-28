a08067aafa38ce88fe920eda95032e1f9f3482e3
shahriaralam465 <shahriaralam465@gamil.com>
2024-01-28 21:56:33 +0600

    added operation.js file

--- operation.js
+++ operation.js
@@ -1,9 +1,39 @@
 var eggPrice =10;
 var onionPrice = 20;
 
-console.log(eggPrice + onionPrice);
-console.log(onionPrice - eggPrice)
-console.log(onionPrice*eggPrice)
-console.log(onionPrice/eggPrice)
+// console.log(eggPrice + onionPrice);
+// console.log(onionPrice - eggPrice)
+// console.log(onionPrice*eggPrice)
+// console.log(onionPrice/eggPrice)
 
-console.log(10/3)
+// console.log(10/3);
+// console.log(40/3);
+
+
+// remainder ভাগশেষ
+
+// console.log(10 % 3);   //what wil be the remainder after dividing 10 by 3
+// console.log(40 % 3);
+
+
+/* ------------------------------------------------------------------------------------ */
+
+var money = 550;
+money = 600;
+
+/* ----------------price related------------------------ */
+var price =35;
+price = price +10;
+
+console.log(price)
+
+// [ এখানে প্রাইজ এর সাথে  ১০ যোগ করা হয়েছে । এবং console.log() দ্বারা বুঝানো হয়েছে এর ফলাফল  ]
+
+/* ----------------------------------------- */
+
+var newPrice = price +50;
+// console.log(newPrice);
+var DiscountedPrice = newPrice -10;
+
+console.log(DiscountedPrice);
+
