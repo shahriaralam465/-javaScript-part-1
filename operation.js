edc651b974e9f4c6b8b1a37998d8591dccc56c44
shahriaralam465 <shahriaralam465@gamil.com>
2024-01-28 21:33:40 +0600

    added operation.js file

---
+++ operation.js
@@ -0,0 +1,9 @@
+var eggPrice =10;
+var onionPrice = 20;
+
+console.log(eggPrice + onionPrice);
+console.log(onionPrice - eggPrice)
+console.log(onionPrice*eggPrice)
+console.log(onionPrice/eggPrice)
+
+console.log(10/3)
